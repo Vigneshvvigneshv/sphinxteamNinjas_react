277a416d4c07dc70404604e09d9ae6872e84d298
Vigneshvvigneshv <vigneshvvigneshv25@gmail.com>
2026-03-19 15:53:24 +0530

    vikcy commited

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -3,19 +3,49 @@ import Layout from '../component/Layout'
 import { FieldContainer, LoginButton, LoginContainer, LoginForm, LoginHeading, LoginInput, LoginLabel } from '../Styles/UserLogin.style'
 
 const UserLogin = () => {
+
+    const[formData, setFormData] = useState({
+        userName: "",
+        password: "",
+        role: "USER"
+  });
+
+   const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8443/sphinx/control/validateuserLogin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
+
   return (
     <Layout>
         <LoginHeading>User Login</LoginHeading>
         <LoginContainer>    
-            <LoginForm>
+            <LoginForm onSubmit={handleSubmit}>
             <FieldContainer>
                 <LoginLabel htmlFor='UserName'>UserName: </LoginLabel>
                 <LoginInput type="text" name='userName' placeholder='Enter your name'></LoginInput>
             </FieldContainer>
 
             <FieldContainer>
-                <LoginLabel htmlFor='UserName'>password:</LoginLabel>
-                <LoginInput type="password" name='userPassword' placeholder='Enter your password' required></LoginInput>
+                <LoginLabel htmlFor='password'>password:</LoginLabel>
+                <LoginInput type="password" name='password' placeholder='Enter your password' required></LoginInput>
 
             </FieldContainer>
 
