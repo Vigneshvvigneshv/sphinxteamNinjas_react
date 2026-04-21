5927b199587e9bdce4d68f42b9ce2ffea5bcd3a5
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-04-21 12:45:19 +0530

    getresult in comleted exam

--- src/component/CompletedExamTable.jsx
+++ src/component/CompletedExamTable.jsx
@@ -3,14 +3,28 @@ import {ButtonContainer, Content, TableRow,AddButton, EditButton} from '../style
 import { LuNotepadText } from "react-icons/lu";
 import { IoMdDownload } from "react-icons/io";
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { apiPost } from '../ApiServices/apiServices';
+import { toast } from 'sonner';
 
 const CompletedExamTable = ({data}) => {
+
+    const partyId=useSelector((state)=>state.userReducer.partyId);
+    console.log("partyId:",partyId)
+    console.log("examId:",data.examId);
+    const fetchResult=async()=>{
+        const response=await apiPost('/exam-result/getexam-result',{partyId:partyId,examId:data.examId});
+        console.log(response);
+        if(response.errorMessage!==undefined){
+            toast.error(response.errorMessage,{position:"top-center"})
+        }
+    }
   return (
     <>
       <TableRow>
         <Content>{data.examName}</Content>
         <ButtonContainer>
-            <AddButton><LuNotepadText/>Result</AddButton>
+            <AddButton onClick={fetchResult}><LuNotepadText/>Result</AddButton>
             <EditButton><IoMdDownload />Certificate</EditButton>
         </ButtonContainer>
       </TableRow>
